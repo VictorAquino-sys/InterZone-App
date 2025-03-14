c201d1bd1a22d0aa837e22192d2c52b88e7a7c7a
Victor A <aquinov@oregonstate.edu>
2025-03-14 13:51:21 -0700

    Allow Free Cropping

--- screens/posts/PostScreen.js
+++ screens/posts/PostScreen.js
@@ -71,7 +71,6 @@ const PostScreen = ({ navigation }) => {
     const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
-        aspect: [16, 9],
         quality: 1,
     });
 
