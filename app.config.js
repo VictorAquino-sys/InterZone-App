929be1a3fbf9dc341d78d21b24a98af664147a36
Victor Aquino <77554989+VictorAquino-sys@users.noreply.github.com>
2025-04-10 15:05:23 -0700

    Increment IOS version bundle

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       sdkVersion: "19.0",
-      buildNumber: "1.0.13",
+      buildNumber: "1.0.14",
       bundleIdentifier: IS_DEV ? "com.interzone.app" : "com.interzone.app",
       runtimeVersion: "sdkVersion",
       usesAppleSignIn: true,
