fe7c1f4a740b70268fba69851c8112a74199322e
Victor A <aquinov@oregonstate.edu>
2025-04-25 20:09:43 -0700

    Disable Proguard temporarily

--- app.config.js
+++ app.config.js
@@ -42,7 +42,7 @@ export default {
       package: IS_DEV ? "com.zhd.interzone" : "com.zhd.interzone",
       versionCode: 22,
       jsEngine: "hermes",
-      enableProguardInReleaseBuilds: true,
+      // enableProguardInReleaseBuilds: true,
       enableShrinkResourcesInReleaseBuilds: true,
       permissions: [
         "android.permission.ACCESS_FINE_LOCATION",
