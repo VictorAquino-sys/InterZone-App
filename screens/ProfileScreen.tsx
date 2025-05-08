deaf3c0a4ca45d2b8d68be533ba7db808003893d
Victor A <aquinov@oregonstate.edu>
2025-05-07 21:39:40 -0700

    feat: polish ProfileScreen with ScrollView fix

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -426,8 +426,8 @@ export default ProfileScreen;
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1,
-        justifyContent: 'center',
+        // flex: 1,
+        // justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
         backgroundColor: 'aliceblue',
